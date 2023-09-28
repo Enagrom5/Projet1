cd396b797cafe497aec96a11be2ea2bf93476c6c
TRISTAN ZVUNKA <tristan.zvunka@gmail.com>
2023-09-28 15:07:25 +0200

    add tristans code in branch

--- Code/script.js
+++ Code/script.js
@@ -1,11 +1,86 @@
+const questionsArray = ["Jour ou Nuit?", "Sport ou Sieste?","Musée ou bronzer?","Blockbuster ou film d'auteur?","Trop chaud ou trop froid?","Plage ou montagne?","Classique ou Métal?","Bière ou vin?","Sucré ou salé?","Humain ou robot?","Livre ou télé?","PC ou console?","Windows ou Linux?","Legolas ou Gimli?","Pilule rouge ou pilule bleue?"]; 
+// Tableau des questions
+// const reponseAArray = ["Jour", "Sport"]; - Tableau des txt réponses A
+// const reponseBArray = ["Nuit", "Sieste"]; - Tableau des txt réponses B
+const imagesA = ["url('../assets/jour.jpg')", "url('../assets/sport.jpg')","url('../assets/musee.jpg')", "url('../assets/blockbuster.jpg')","url('../assets/chaud.jpg')", "url('../assets/plage.jpg')", "url('../assets/classique.jpg')","url('../assets/biere.jpg')", "url('../assets/sucre.jpg')", "url('../assets/humain.jpg')","url('../assets/livre.jpg')", "url('../assets/pc.jpg')", "url('../assets/windows.png')", "url('../assets/legolas.jpg')", "url('../assets/rouge.jpg')"] 
+// Tableau des images A
+const imagesB = ["url('../assets/nuit.jpg')", "url('../assets/sieste.jpg')","url('../assets/bronzage.jpg')", "url('../assets/auteur.jpg')","url('../assets/froid.jpg')", "url('../assets/montagne.jpg')", "url('../assets/metal.jpg')","url('../assets/vin.jpg')", "url('../assets/sale.jpg')", "url('../assets/robot.jpg')","url('../assets/tele.jpg')", "url('../assets/console.jpg')", "url('../assets/linux.jpg')", "url('../assets/gimli.jpg')", "url('../assets/bleu.jpg')"] 
+// Tableau des images B
+
+const question = document.querySelector('.question'); // Sélectionne en JS la balise question
+const reponseA = document.querySelector('.cardAnswerA'); // Sélectionne en JS la balise réponse A
+const reponseB = document.querySelector('.cardAnswerB'); // Sélectionne JS la balise réponse B
+const buttonStart = document.querySelector('.buttonStart')
+
+let i = 0;
+
+/**
+ * Fonction qui permet la suppression du bouton "démarrer le quiz"
+ */
+function removeButton(){
+ buttonStart.remove();
+ reponseA.style.display= "block";
+ reponseB.style.display= "block"
+ question.style.display= "block";
+ displayQuestion(i);
+}
+
+/**
+ * Fonction qui permet de passer à la question suivante
+ */
+function displayQuestion(idQuestion) {
+    question.innerHTML = questionsArray[idQuestion];  // Affiche la question dans le tableau questionsArray
+    //reponseA.innerHTML = reponseAArray[idQuestion]; - Affiche le texte de la réponse A via le tableau des txt réponses A
+    //reponseB.innerHTML = reponseBArray[idQuestion]; - Affiche le texte de la réponse B via le tableau des txt réponses B
+    reponseA.style.backgroundImage = imagesA[idQuestion]; // Affiche l'image A dans le cadre A via le tableau des images A
+    reponseB.style.backgroundImage = imagesB[idQuestion]; // Affiche l'image B dans le cadre B via le tableau des images B
+}
+/**
+ * Suppression du bouton de lancement de quiz
+ */
+buttonStart.addEventListener('click', function() {
+    removeButton();
+})
+/**
+ * Passe à la question suivante (bouton A)
+ */
+reponseA.addEventListener('click', function () {
+    // permet de passer à la question suivante
+    i++;
+
+    if (i < questionsArray.length) {
+        displayQuestion(i);
+    } else {
+        // tu affiches la destination de rêve !
+    }
+})
+
+/**
+ * Passe à la question suivante (bouton B)
+ */
+reponseB.addEventListener('click', function () {
+    // permet de passer à la question suivante
+    i++;
+
+    if (i < questionsArray.length) {
+        displayQuestion(i);
+    } else {
+        // tu affiches la destination de rêve !
+    }
+})
+/**
+ * Ce qui permet de ne pas afficher les cartes au chargement
+ */
+onload = (event) => { question.style.display="none", reponseA.style.display= "none", reponseB.style.display= "none"}
+/** -----------------------------------------------
+
 const questions = document.querySelector('.Questions');
 const body = document.querySelector("body")
 questions.style.backgroundColor = "#151515"
 
 
-/**
- * Toutes les questions
- */
+
+
 
 const questionReponse = [
     {
@@ -130,9 +205,9 @@ const questionReponse = [
     }
 ]
 
-/**
+
  * La fonction createCard, permet de créer une carte en fonction des parametres reçu
- */
+
 
 function createCard(quest, image1Url, image2Url, reponse1, reponse2) {
 
@@ -147,7 +222,7 @@ function createCard(quest, image1Url, image2Url, reponse1, reponse2) {
     const question = document.createElement("h2");
     question.classList.add("question")
     question.textContent = quest;
-    
+
     questionContainer.appendChild(question);
 
     const cardsContainers = document.createElement("span");
@@ -158,10 +233,14 @@ function createCard(quest, image1Url, image2Url, reponse1, reponse2) {
 
     const cardContainer1 = document.createElement("span");
     cardContainer1.classList.add("card-container1");
+    cardContainer1.style.margin = '1rem';
+    cardContainer1.style.width = "49%";
     cardsContainers.appendChild(cardContainer1);
 
     const cardContainer2 = document.createElement("span");
     cardContainer2.classList.add("card-container2");
+    cardContainer2.style.margin = '1rem';
+    cardContainer2.style.width = "49%";
     cardsContainers.appendChild(cardContainer2);
 
     const cardContainerBody1 = document.createElement("div");
@@ -230,13 +309,10 @@ function createCard(quest, image1Url, image2Url, reponse1, reponse2) {
 
 
 for (const element of questionReponse) {
-    //button1, button2// 
+    //button1, button2//
     createCard(element.question, element.picture1, element.picture2, element.reponse1, element.reponse2);
-}
 
 
-
-/**
  * Version 2 avec l'affichage d'une question par une question
  */
 // let i = 0;
