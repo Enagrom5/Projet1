0a3cb821ed80df3766e36b5796710d97638a80c6
MORGANE DEBARGE <morganedebarge@msn.com>
2023-09-29 15:11:02 +0200

    merge js V1 dans la V2

--- Code/script.js
+++ Code/script.js
@@ -1,3 +1,11 @@
+const destination = document.querySelector(".destination");
+const buttonResult = document.querySelector(".button-result" );
+const titre = document.querySelector(".titreDestination");
+const destinationImage = document.querySelector(".destinationImage");
+const destinationDescription = document.querySelector(".destinationDescription");
+const buttonRestart = document.querySelector(".try-again")
+const accueil = document.querySelector(".startQuiz")
+
 
 const questionsArray = ["Jour ou Nuit?", "Sport ou Sieste?","Musée ou bronzer?","Blockbuster ou film d'auteur?","Trop chaud ou trop froid?","Plage ou montagne?","Classique ou Métal?","Bière ou vin?","Sucré ou salé?","Humain ou robot?","Livre ou télé?","PC ou console?","Windows ou Linux?","Legolas ou Gimli?","Pilule rouge ou pilule bleue?"]; 
 // Tableau des questions
@@ -42,17 +50,19 @@ function displayQuestion(idQuestion) {
 buttonStart.addEventListener('click', function() {
     removeButton();
 })
+let calculNumber = 0;
 /**
  * Passe à la question suivante (bouton A)
  */
 reponseA.addEventListener('click', function () {
     // permet de passer à la question suivante
     i++;
-
+    calculNumber += 5;
+    console.log(calculNumber)
     if (i < questionsArray.length) {
         displayQuestion(i);
     } else {
-        // tu affiches la destination de rêve !
+        buttonResult.style.display="flex";
     }
 })
 
@@ -62,15 +72,137 @@ reponseA.addEventListener('click', function () {
 reponseB.addEventListener('click', function () {
     // permet de passer à la question suivante
     i++;
-
+    calculNumber += 2;
+    console.log(calculNumber)
     if (i < questionsArray.length) {
         displayQuestion(i);
     } else {
-        // tu affiches la destination de rêve !
+       buttonResult.style.display="flex";
     }
 })
+
+
 /**
  * Ce qui permet de ne pas afficher les cartes au chargement
  */
 onload = (event) => { question.style.display="none", reponseA.style.display= "none", reponseB.style.display= "none"}
 
+
+
+
+/* Tableau pour les destinations*/
+let randomNumber = Math.floor(Math.random() * 14);
+console.log(randomNumber);
+
+const destinationList = [{
+    name: "Un monatère",
+    description: "Connu pour leur fête, les moines vous assureront un séjour inoubliable en vous contant les histoires les plus palpitantes!",
+    img: "../assets/Destination/monasteredusilence.jpg",
+},
+{
+    name: "Le Mordor",
+    description: "Bien connu pour sa population un poil rustique, et ses paysages luxuriants, le Mordor est un pays ou il fait bon vivre. C'est la saison idéale! Après il fera un peu trop froid.",
+    img: "../assets/Destination/mordor1.jpg",
+},
+{
+    name: "Hoth la planète de glace",
+    description: "Attendez vous à une expérience chaleureuse! La cuisine locale vous coupera le souffle!",
+    img: "../assets/Destination/hothplanet.jpg",
+},
+{
+    name: "Bergerie dans le Larzac",
+    description: "Pour se reposer quoi de mieux que le travail de la terre!",
+    img: "../assets/Destination/bergeriedepierre.jpg",
+},
+{
+    name: "Azkaban",
+    description: "The place to be pour profiter de toute la liberté possible!",
+    img: "../assets/Destination/Azkaban.png",
+},
+{
+    name: "Le monde des télétubbies",
+    description: "Endroit paisible et calme. Vous pourrez observer en toute tranquillité les autochtones",
+    img: "../assets/Destination/teletubbies1.jpg",
+},
+{
+    name: "Centre de désintoxication",
+    description: "Dernier lieu à la mode pour partir en vacances. Méfiance tout de même n'acceptez aucune pilule qu'on vous proposerait!",
+    img: "../assets/Destination/centredesintox.jpg",
+},
+{
+    name: "Camping nudiste",
+    description: "Si vous aimez les séjours simples, celui ci est fait pour vous. Attention Sortez couvert!",
+    img: "../assets/Destination/plagenudiste1.jpg",
+},
+{
+    name: "Bivouac chez les marcheurs blancs",
+    description: "Qui n'a jamais rêvé de franchir le mur? Ce bivouac est fait pour vous.",
+    img: "../assets/Destination/beyondthewall.png",
+},
+{
+    name: "Séjour linguistique chez les Goa'uld ",
+    description: "Population extrêmement amicale, avec eux c'est presque symbiotique. Veillez à ne pas en ramener un en rentrant!",
+    img: "../assets/Destination/stargate.jpg",
+},
+{
+    name: "Hôtel Cortez",
+    description: "Séjour à vous glacer le sang! Showcase de Lady Gaga",
+    img: "../assets/Destination/hotelcortez.png",
+},
+{
+    name: "Le Tartare",
+    description: "Un incroyable supplice!",
+    img: "../assets/Destination/tartare.png",
+},
+{
+    name: "Hôpital psychitrique Ashecliffe",
+    description: "Tellement intense qu'on en perd la boule!",
+    img: "../assets/Destination/shutterisland.png",
+},
+{
+    name: "Croisière sur le Titanic",
+    description: "Profitez de leur animation, les pieds dans l'eau!",
+    img: "../assets/Destination/titanic2.jpg",
+}]
+
+
+
+/**
+ * CHANGER LES PAGES 
+ *  */
+
+
+
+
+
+/**
+ * quiz => result 
+ *  */
+const questions = document.querySelector(".Questions");
+buttonResult.addEventListener("click", function () {
+    questions.style.display = "none";
+    destination.style.display = "flex";
+    titre.textContent = `${destinationList[randomNumber].name}`;
+    destinationImage.src = `${destinationList[randomNumber].img}`;
+    destinationDescription.textContent = `${destinationList[randomNumber].description}`;
+
+})
+const submitButton = document.querySelector(".submitButton");
+
+submitButton.addEventListener("click", function () {
+    alert("Merci de votre intéret. Nos équipes reviendront vers vous au plus vite!")
+    event.preventDefault()
+});
+/**
+ * result => accueil 
+ *  */
+buttonRestart.addEventListener("click", function () {
+    questions.style.display = "none";
+    buttonContainer.style.display = "none";
+    destination.style.display = "none";
+    buttonRestart.style.display = "none";
+    accueil.style.display = "flex";
+    buttonStart.style.display = "flex";
+
+
+})
